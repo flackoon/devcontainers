769de63a7ce21de9403243dcc0944b350b1871bd
flackoon <nkatrandjiev@gmail.com>
2026-01-28 14:57:26 +0200

    post-install: remove obsolete stuff and make it compact

--- install-tools.sh
+++ install-tools.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/fish
 
-# Some manual exports
-## This makes pnpm installations to be available globally
-echo "export PNPM_HOME=\\"/home/vscode/.local/share/pnpm\\"" >> ~/.zshrc
-## This is the default path for foundry's binaries
-echo "export FOUNDRY=\\"/home/vscode/.foundry/bin\\"" >> ~/.zshrc
-## And here we just add both of them to PATH.
-echo "export PATH=\\"\\$PATH:\\$PNPM_HOME:\\$FOUNDRY\\"" >> ~/.zshrc
-
-# Install hardhat
-# pnpm install hardhat -g
-
 # Install solc-select
 pipx install solc-select
 
@@ -24,11 +13,13 @@ curl https://cursor.com/install -fsS | bash
 
 # Install Brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo >> /home/vscode/.zshrc
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv zsh)"' >> /home/vscode/.zshrc
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv zsh)"
 
-# Install essentials for working with Git
+# Install essentials for working with Git (and other niceties)
 brew install git-delta tig less scmpuff zsh-autosuggestions vim
 
-echo 'eval "$(scmpuff init -s)"' >> /home/vscode/.zshrc
+# Copy over zsh and tig configs instead of echo-ing like a mad man
+curl -L https://raw.githubusercontent.com/flackoon/devcontainers/refs/heads/master/zshrc -o ~/.zshrc
+curl -L https://raw.githubusercontent.com/flackoon/devcontainers/refs/heads/master/tigrc -o ~/.tigrc
+
+source ~/.zshrc
